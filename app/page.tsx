1be3e4a8da791da5c379dd9868716eb05f3605ca
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2026-03-13 00:37:57 +0600

    HomePage implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import BookCard from "@/components/BookCard"
+import HeroSection from "@/components/HeroSection"
+import { sampleBooks } from "@/lib/constants"
+
 
 const page = () => {
   return (
-    <div>
-      {/* <h1 className="text-2xl underline">BookChat</h1> */}
-    </div>
+    <main className="wrapper container">
+      <HeroSection/>
+
+     <div className="library-books-grid">
+                {sampleBooks.map((book) => (
+                    <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
+                ))}
+            </div>
+        </main>
   )
 }
 
